4255a0531d993ba4a55dba7c8cc63213e6979bd4
fsabbane <faycal.sabbane@visionconsulting.de>
2026-05-10 18:43:19 +0200

    fix: remove duplicate imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
 import { LanguageProvider } from './contexts/LanguageContext'
